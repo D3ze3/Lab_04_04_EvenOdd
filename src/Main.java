4e4190e941cba3495a3044b0ce49b7a835ebb470
D3ze3 <currysplash3@gmail.com>
2025-02-05 12:32:16 -0800

    Task4

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Declare and initialize an integer
+        int numToExamine = 2;
+
+        // Determine if the number is odd or even
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is Even.");
+        } else {
+            System.out.println(numToExamine + " is Odd");
+        }
+    }
+}
+
